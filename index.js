73c08ed729f9bf75bd86725cd324fe05a5c7f70f
Jacqueline Watson-Sinclair <faycello75@gmail.com>
2023-03-24 00:54:14 -0400

    both for and while assignments completed successfully

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let message = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`));  
+  }
+  return message;
+}
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(number) {  
+  while (number >= 0) {
+  console.log(number);
+  number--;
+}
+}
